49468d412f6b2683c57a64959964789a17614838
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2023-01-05 02:34:20 +0100

    remove #[Route()], it's not required to reproduce the issue

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route(
-        '/about',
-        name: 'true_badges_about'
-    )]
     public function aboutAction()
     {
         return $this->render('about.html.twig');
